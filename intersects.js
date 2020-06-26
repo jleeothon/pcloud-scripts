9ac45e892bd2bf246d67cae8f3a57f67de76ab5a
Johnny Lee-Othon <jleeothon@icloud.com>
2020-06-26 11:52:08 +0200

    Initial commit with intersects.js

---
+++ intersects.js
@@ -0,0 +1,56 @@
+const pcloudSdk = require('pcloud-sdk-js');
+const pMap = require('p-map');
+
+const client = pcloudSdk.createClient(process.env.ACCESS_TOKEN);
+
+function intersects(a, b) {
+	for (const v of a.values()) {
+		if (b.has(v)) {
+			return true;
+		}
+	}
+
+	return false;
+}
+
+function mergeNames(hashesPerName) {
+	const result = [];
+	for (const {name, hashes} of hashesPerName) {
+		const resultItem = result.find(i => intersects(hashes, i.hashUnion));
+		if (!resultItem) {
+			result.push({names: [name], hashUnion: hashes});
+			continue;
+		}
+
+		resultItem.names.push(name);
+		hashes.forEach(h => resultItem.hashUnion.add(h));
+	}
+
+	return result;
+}
+
+async function run() {
+	const folderId = Number.parseInt(process.env.FOLDER_ID, 10);
+	const allFiles = await client.listfolder(folderId, {recursive: true});
+	const goodFolders = allFiles.contents.filter(f => f.name.match(/^[\w-]+ \d+$/));
+
+	const hashesPerName = await pMap(goodFolders, async f => {
+		const name = f.name;
+		const hashList = f.contents.filter(f => f.contenttype === 'image/jpeg').map(f => f.hash);
+		const hashes = new Set(hashList);
+		return {name, hashes};
+	});
+
+	// [{names: [], hashUnion: Set}, ...]
+	const nonSingleMergedNames = mergeNames(hashesPerName);
+	const mergedNames = nonSingleMergedNames.filter(({names}) => names.length > 1);
+
+	// [[name1, name2, name3], ...]
+	const onlyNames = mergedNames.map(({names}) => names);
+
+	console.log(onlyNames);
+}
+
+run().catch(error => {
+	console.error(error);
+});
